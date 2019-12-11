f7190e460f8139c93b5c39fe494e56b19f2dd8e8
Robert Wessels <robertinant@yahoo.com>
2019-12-11 10:24:40 -0800

    Bump version to E23

--- arduino-core/src/processing/app/BaseNoGui.java
+++ arduino-core/src/processing/app/BaseNoGui.java
@@ -42,10 +42,10 @@ public class BaseNoGui {
 
   /** Version string to be used for build */
   public static final int REVISION = 10811;
-  public static final int EREVISION = 22;
+  public static final int EREVISION = 23;
   /** Extended version string displayed on GUI */
   public static final String VERSION_NAME = "1.8.11";
-  public static final String EVERSION_NAME = "22";
+  public static final String EVERSION_NAME = "23";
   public static final String VERSION_NAME_LONG;
   public static final String EVERSION_NAME_LONG;
 
